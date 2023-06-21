bb9697f32d5ed2871b4287b5cf62f4385aed88a3
wogud1220 <yoonwogud@gmail.com>
2023-06-21 10:02:51 +0900

    asdf

--- FinalProject/src/OrderNumber.java
+++
@@ -1,7 +0,0 @@
-
-public interface OrderNumber {
-	public abstract int getOrderNumber();
-	/* 번호표 받을 수 있도록 abstract라서 내용이 없음 이름만잇음
-	 * 이 함수 정의해야함
-	 */
-}
--- FinalProject/src/PosDevice.java
+++
@@ -1,9 +0,0 @@
-
-public class PosDevice implements PurchaseOrder{
-	@Override
-	public void sendOrder(int OrderNum, String menuName, int pcs) {
-		System.out.println("Order["+OrderNum+"] : "+menuName+"("+pcs+")");
-		return;
-	};
-
-}
